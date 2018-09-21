358ce4aa7160c0236bb3e5a986351fefdb0d8635
Kizito Akhilome <hovkard@gmail.com>
2018-09-21 03:51:23 +0100

    Setup postgres pool and query db for all recipes

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const pgPassword = require('./secrets');
 
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'recipebookdb',
+  password: pgPassword
+});
 
 const app = express();
 
@@ -8,8 +16,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/recipes', (req, res) => {
-  res.send('Making this an API')
+app.get('/recipes', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM recipes');
+    res.json({ recipes: result.rows });
+  } catch (err) {
+    res.json({err});
+  }
 });
 
 // Setup Server Port
