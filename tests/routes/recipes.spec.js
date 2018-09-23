5aa9be317b26829c9b77c00be1bf014390ed2176
Kizito Akhilome <hovkard@gmail.com>
2018-09-23 22:02:53 +0100

    Write tests for GET /recipe/:id

--- tests/routes/recipes.spec.js
+++ tests/routes/recipes.spec.js
@@ -4,7 +4,7 @@ const chaiHttp = require('chai-http');
 const dirtyChai = require('dirty-chai');
 
 const app = require('./../../app.js');
-const { populateTables } = require('../seed/seed');
+const { seedData, populateTables } = require('../seed/seed');
 
 chai.use(chaiHttp);
 chai.use(dirtyChai);
@@ -29,6 +29,42 @@ describe('GET /recipes', () => {
       .end((err, res) => {
         res.status.should.equal(200);
         res.body.recipes.length.should.equal(2);
+        done();
+      });
+  });
+});
+
+describe('GET /recipes/:id', () => {
+  const validId = Math.ceil(Math.random() * seedData.recipes.length);
+  const invalidId = seedData.recipes.length + 1;
+  it('should return a recipe if valid recipe id is provided', (done) => {
+    chai.request(app)
+      .get(`/api/v1/recipes/${validId}`)
+      .end((err, res) => {
+        if (err) done(err);
+        res.status.should.equal(200);
+        res.body.should.be.an('object').that.has.property('recipe');
+        res.body.recipe.should.contain.keys(['name', 'ingredients', 'directions']);
+        done();
+      });
+  });
+
+  it('should return a 404 if invalid recipe id is provided', (done) => {
+    chai.request(app)
+      .get(`/api/v1/recipes/${invalidId}`)
+      .end((err, res) => {
+        if (err) done(err);
+        res.status.should.equal(404);
+        done();
+      });
+  });
+
+  it('should return a 400 status if invalid id type is used', (done) => {
+    chai.request(app)
+      .get('/api/v1/recipes/thisIsWrong')
+      .end((err, res) => {
+        if (err) done(err);
+        res.status.should.equal(400);
         done();
       });
   });
