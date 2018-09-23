8d29ad2f1d10e1d4970943ef86a5148f7fe218bf
Kizito Akhilome <hovkard@gmail.com>
2018-09-23 19:54:23 +0100

    Write tests for GET /recipes

--- tests/routes/recipes.spec.js
+++ tests/routes/recipes.spec.js
@@ -11,8 +11,25 @@ chai.use(dirtyChai);
 
 beforeEach(populateTables);
 
-describe('Dummy test', () => {
-  it('should pass', (done) => {
-    done();
+describe('GET /recipes', () => {
+  it('should return an array of existing recipes', (done) => {
+    chai.request(app)
+      .get('/api/v1/recipes')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('recipes');
+        res.body.recipes.should.be.an('array');
+        done();
+      });
+  });
+
+  it('should return the correct amount of recipes', (done) => {
+    chai.request(app)
+      .get('/api/v1/recipes')
+      .end((err, res) => {
+        res.status.should.equal(200);
+        res.body.recipes.length.should.equal(2);
+        done();
+      });
   });
 });
