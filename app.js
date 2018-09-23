ab8754d6ee99068fa6d89ee85a862dd8849f3681
Kizito Akhilome <hovkard@gmail.com>
2018-09-23 15:24:21 +0100

    Version API and make use of express router

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const pool = new Pool({ connectionString: process.env.DB_URL });
+const recipeRouter = require('./routes/recipeRouter');
 
 const app = express();
 
@@ -13,51 +8,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/recipes', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM recipes');
-    res.json({ recipes: result.rows });
-  } catch (err) {
-    res.json({err});
-  }
-});
-
-app.post('/recipes/add', async (req, res) => {
-  const { name, ingredients, directions } = req.body;
-  const text = 'INSERT INTO recipes(name, ingredients, directions) VALUES ($1, $2, $3) RETURNING *';
-
-  try {
-    const result = await pool.query(text, [name, ingredients, directions]);
-    res.json({ recipe: result.rows });
-  } catch (err) {
-    res.status(400).json({err});
-  }
-});
-
-app.delete('/recipes/:id', async (req, res) => {
-  const { id } = req.params;
-  const text = 'DELETE FROM recipes WHERE id=($1)';
-
-  try {
-    await pool.query(text, [id]);
-    res.status(204).json();
-  } catch (err) {
-    res.status(400).json({err});
-  }
-});
-
-app.put('/recipes/:id', async (req, res) => {
-  const { id } = req.params;
-  const { name, ingredients, directions } = req.body;
-  const text = 'UPDATE recipes SET name=$1, ingredients=$2, directions=$3 WHERE id=$4 RETURNING *';
-  try {
-    const existingRecipe = (await pool.query('SELECT * FROM recipes WHERE id=$1', [id])).rows[0];
-    const updatedRecipe = (await pool.query(text, [name || existingRecipe.name, ingredients || existingRecipe.ingredients, directions || existingRecipe.directions, id])).rows[0];
-    res.json({ updatedRecipe });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+// Recipes 
+app.use('/api/v1/recipes', recipeRouter);
 
 // Setup Server Port
 app.listen(3000, () => console.log('app running on 3000'));
---
+++ routes/recipeRouter.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const pool = new Pool({ connectionString: process.env.DB_URL });
+
+router.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM recipes');
+    res.json({ recipes: result.rows });
+  } catch (err) {
+    res.json({err});
+  }
+});
+
+router.post('/add', async (req, res) => {
+  const { name, ingredients, directions } = req.body;
+  const text = 'INSERT INTO recipes(name, ingredients, directions) VALUES ($1, $2, $3) RETURNING *';
+
+  try {
+    const result = await pool.query(text, [name, ingredients, directions]);
+    res.json({ recipe: result.rows });
+  } catch (err) {
+    res.status(400).json({err});
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const text = 'DELETE FROM recipes WHERE id=($1)';
+
+  try {
+    await pool.query(text, [id]);
+    res.status(204).json();
+  } catch (err) {
+    res.status(400).json({err});
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, ingredients, directions } = req.body;
+  const text = 'UPDATE recipes SET name=$1, ingredients=$2, directions=$3 WHERE id=$4 RETURNING *';
+  try {
+    const existingRecipe = (await pool.query('SELECT * FROM recipes WHERE id=$1', [id])).rows[0];
+    const updatedRecipe = (await pool.query(text, [name || existingRecipe.name, ingredients || existingRecipe.ingredients, directions || existingRecipe.directions, id])).rows[0];
+    res.json({ updatedRecipe });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
