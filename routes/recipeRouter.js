c991e23ea641f0fb0eee32d7001e1aec5a9314b9
Kizito Akhilome <hovkard@gmail.com>
2018-09-23 15:45:53 +0100

    Install and setup esliint
    - make use of Airbnb javascript styleguide

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res) => {
     const result = await pool.query('SELECT * FROM recipes');
     res.json({ recipes: result.rows });
   } catch (err) {
-    res.json({err});
+    res.json({ err });
   }
 });
 
@@ -18,7 +18,7 @@ router.post('/add', async (req, res) => {
     const result = await pool.query(text, [name, ingredients, directions]);
     res.json({ recipe: result.rows });
   } catch (err) {
-    res.status(400).json({err});
+    res.status(400).json({ err });
   }
 });
 
@@ -30,7 +30,7 @@ router.delete('/:id', async (req, res) => {
     await pool.query(text, [id]);
     res.status(204).json();
   } catch (err) {
-    res.status(400).json({err});
+    res.status(400).json({ err });
   }
 });
 
@@ -40,7 +40,13 @@ router.put('/:id', async (req, res) => {
   const text = 'UPDATE recipes SET name=$1, ingredients=$2, directions=$3 WHERE id=$4 RETURNING *';
   try {
     const existingRecipe = (await pool.query('SELECT * FROM recipes WHERE id=$1', [id])).rows[0];
-    const updatedRecipe = (await pool.query(text, [name || existingRecipe.name, ingredients || existingRecipe.ingredients, directions || existingRecipe.directions, id])).rows[0];
+    const values = [
+      name || existingRecipe.name,
+      ingredients || existingRecipe.ingredients,
+      directions || existingRecipe.directions,
+      id,
+    ];
+    const updatedRecipe = (await pool.query(text, values)).rows[0];
     res.json({ updatedRecipe });
   } catch (err) {
     res.status(400).json(err);
